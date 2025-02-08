07354047712ec97547d85ad56a5896fc110dd17d
Shubham-Vandara <shubhvandara@gmail.com>
2025-02-08 17:58:38 +0530

    first commit

---
+++ src/main/java/com/example/frontcrud/controller/FrontUserController.java
@@ -0,0 +1,63 @@
+package com.example.frontcrud.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.frontcrud.entities.FrontUser;
+import com.example.frontcrud.service.FrontUserServiceImpl;
+
+@RestController
+@RequestMapping("/frontUser")
+public class FrontUserController {
+
+    @Autowired
+    private FrontUserServiceImpl frontUserService;
+
+    // Fetch all FrontUser entities
+    @GetMapping
+    public List<FrontUser> getAllFrontUser() {
+        return frontUserService.getAllFrontUser();
+    }
+
+    // Fetch a specific FrontUser by ID
+    @GetMapping("/{id}")
+    public Optional<FrontUser> getFrontUserById(@PathVariable Integer id) {
+        return frontUserService.getFrontUserById(id);
+    }
+
+    // Create a new FrontUser entity
+    @PostMapping
+    public FrontUser createUser(@RequestBody FrontUser user) {
+        return frontUserService.saveFrontUser(user);
+    }
+    
+ // Create a new FrontUser entity
+    @PostMapping("/addUsers")
+    public List<FrontUser> createAllUser(@RequestBody List<FrontUser> user) {
+        return frontUserService.saveAllFrontUser(user);
+    }
+
+    // Update an existing FrontUser entity
+    @PutMapping("/{id}")
+    public FrontUser updateUser(@PathVariable Integer id, @RequestBody FrontUser user) {
+        user.setId(id);
+        return frontUserService.saveFrontUser(user);
+    }
+
+    // Delete a FrontUser entity
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        frontUserService.deleteFrontUser(id);
+    }
+
+    // Return a blank FrontUser object (example method)
+    private FrontUser frontUser = new FrontUser();
+
+    @GetMapping("/getObject")
+    public FrontUser getUserObject() {
+        return frontUser;
+    }
+}
